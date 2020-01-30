be4538a8d0d3ae35d02a334f833812308b1117c7
Deep27 <geep2795@gmail.com>
2020-01-30 14:25:44 +0300

    Filter for showing only favorites or all.

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,16 +1,50 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
 
 import 'package:shop_app/main.dart';
 
 import 'package:shop_app/widgets/product/products_grid_widget.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  bool _showOnlyFavorites = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text(appName)),
-      body: ProductsGridWidget(),
+      appBar: AppBar(
+        title: const Text(appName),
+        actions: <Widget>[
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (ctx) => [
+              PopupMenuItem(
+                child: const Text('Only Favorites'),
+                value: FilterOptions.showFavorites,
+              ),
+              PopupMenuItem(
+                child: const Text('Show All'),
+                value: FilterOptions.showAll,
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.showFavorites) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+          ),
+        ],
+      ),
+      body: ProductsGridWidget(_showOnlyFavorites),
     );
   }
 }
+
+enum FilterOptions { showFavorites, showAll }
