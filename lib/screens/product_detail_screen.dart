13bfa2467fa6183af8c74d12c3fc86f2366be5b5
Deep27 <geep2795@gmail.com>
2020-01-31 17:52:39 +0300

    Update ProductDetailScreen.

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -4,17 +4,46 @@ import 'package:shop_app/providers/product.dart';
 import 'package:shop_app/providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-
-  static const routeName = '/product-detail'; 
+  static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
     // listen = false - don't update widget if list has changed
-    Product product = Provider.of<Products>(context, listen: false).findById(productId);
+    Product product =
+        Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '\$${product.price}',
+              style: const TextStyle(color: Colors.grey, fontSize: 20),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
