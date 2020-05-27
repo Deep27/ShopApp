3d217a4a6fd2cacc3b4cb92c40d4473c8bd302fd
Deep27 <geep2795@gmail.com>
2020-05-27 20:13:30 +0300

    Improved provider.

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -3,8 +3,7 @@ import 'package:shop_app/data/products.dart';
 import 'package:shop_app/models/product.dart';
 
 class Products with ChangeNotifier {
-
-  List<Product>  _items = products;
+  List<Product> _items = products;
 
   List<Product> get items => [..._items];
 
@@ -12,4 +11,6 @@ class Products with ChangeNotifier {
     _items.add(p);
     notifyListeners();
   }
+
+  Product findById(String id) => _items.firstWhere((item) => item.id == id);
 }
