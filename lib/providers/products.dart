e5e43c14a5b088ae53f15604d2ad45b51cb13c0c
Deep27 <geep2795@gmail.com>
2020-05-28 19:09:45 +0300

    Filtering items in ProductsGrid.

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -5,12 +5,32 @@ import 'package:shop_app/providers/product.dart';
 class Products with ChangeNotifier {
   List<Product> _items = products;
 
-  List<Product> get items => [..._items];
+//  bool _showFavoritesOnly = false;
+
+  List<Product> get items {
+//    if (_showFavoritesOnly) {
+//      return _items.where((item) => item.isFavorite).toList();
+//    }
+    return [..._items];
+  }
 
   void addProduct(Product p) {
     _items.add(p);
     notifyListeners();
   }
 
+  List<Product> get favoriteItems =>
+      _items.where((item) => item.isFavorite).toList();
+
   Product findById(String id) => _items.firstWhere((item) => item.id == id);
+
+//  void showFavoritesOnly() {
+//    _showFavoritesOnly = true;
+//    notifyListeners();
+//  }
+//
+//  void showAll() {
+//    _showFavoritesOnly = false;
+//    notifyListeners();
+//  }
 }
