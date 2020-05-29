598f09dd42ab4e4bf42e6302c26dcf924de4271e
Deep27 <geep2795@gmail.com>
2020-05-29 18:47:15 +0300

    Added cart screen.

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/cart.dart';
+
+class CartScreen extends StatelessWidget {
+  static const routeName = '/cart';
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your cart'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            margin: const EdgeInsets.all(15),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  const Text('Total', style: const TextStyle(fontSize: 20)),
+                  Spacer(),
+                  Chip(
+                    label: Text(
+                      '\$${cart.totalAmount}',
+                      style: TextStyle(
+                          color: Theme.of(context)
+                              .primaryTextTheme
+                              .headline6
+                              .color),
+                    ),
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  FlatButton(
+                    child: const Text('ORDER NOW'),
+                    onPressed: () {},
+                    textColor: Theme.of(context).primaryColor,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
